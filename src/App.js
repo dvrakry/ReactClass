e4c2fa1d88ef273d0b1c2b5a1c036ec208f2dfa8
rak <dv.rakry@gmail.com>
2022-03-17 09:20:12 +0900

    2022-03-17

--- src/App.js
+++ src/App.js
@@ -7,9 +7,19 @@ import './App.css';
 function App() {
 
   let [글제목,글제목변경] = useState(['강북 맛집 추천', '강남 우동 맛집', '강남 초밥 맛집']);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0,0,0]);
 
   let [modal, modal변경] = useState(false);
+
+  function 반복된UI(){
+    var 어레이 = [];
+    for (var i = 0; i < 3; i++){
+      어레이.push(<div>안녕</div>)
+    }
+
+    return 어레이;
+  }
+
 
   let posts = '강남 고기 맛집';
 
@@ -26,7 +36,7 @@ function App() {
       </div>
       {/* <button onClick={ 제목바꾸기 }>버튼</button> */}
       <div className='list'>
-        <h4>{ 글제목[0] } <span onClick={ ()=>{ 따봉변경(따봉 + 1) } }>👍</span> {따봉} </h4>
+        <h4>{ 글제목[0] }  </h4>
         <p>2월 17일 발행</p>
         <hr/>
       </div>
@@ -41,11 +51,27 @@ function App() {
         <hr/>
       </div>
 
+      {/* { 반복된UI() } */}
+
+      {
+        글제목.map(function(a){
+          return (
+          <div className='list' key={ a }>
+            <h4>{ a } <span onClick={ ()=>{ 따봉.map(function(i){
+              return i+1;
+            })} }>👍</span> {따봉}</h4>
+            <p>2월 18일 발행</p>
+            <hr/>
+          </div>
+          )
+        })
+      }
+
       <button onClick={()=>{modal변경(true)}}>버튼</button>
 
       {
         modal == true
-        ? <Modal/>
+        ? <Modal 글제목={글제목}/> 
         : null
       }
       
@@ -53,10 +79,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
     <div className="modal">
-      <h2>제목</h2>
+      <h2>{ props.글제목[1] }</h2>
       <p>날짜</p>
       <p>상세내용</p>
     </div>
